b15bf58691e438f1643fdeb0235680b7e42f63c9
river-veek <61172126+river-veek@users.noreply.github.com>
2020-04-15 23:57:31 -0700

    Created during Calculus 253.

---
+++ summation_error.py
@@ -0,0 +1,15 @@
+def find_N(d, p):
+    """
+    Estimate Sum(1 / n**p) from n = 1 to infinity.
+    d = error bound (10**(-d))
+    p = exponent in telescoping equation
+    Finds N value needed to compute sum with error of d.
+    """
+    import math
+    N = 10**(math.log(10**d / (p - 1), 10)/ (p - 1))
+    N = math.ceil(N). # find nearest (larger) whole number
+    print(f"N = {N}")
+    return None
+
+print("Example:")
+findN(5, 7)
